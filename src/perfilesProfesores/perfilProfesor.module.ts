97f167d776a0673cf0145f1b8eb92c72492c6d4c
PMoyanoAiraldi <pmoyano497@gmail.com>
2024-11-22 19:21:30 -0300

    Se añadio UsuarioModule y la entidad Usuario en el module de PerfilProfesor

--- src/perfilesProfesores/perfilProfesor.module.ts
+++ src/perfilesProfesores/perfilProfesor.module.ts
@@ -4,15 +4,20 @@ import { Clase } from "src/clases/clase.entity";
 import { PerfilProfesor } from "./perfilProfesor.entity";
 import { PerfilesProfesoresService } from "./perfilProfesor.service";
 import { PerfilesProfesoresController } from "./perfilProfesor.controller";
+import { UsuariosService } from "src/usuarios/usuario.service";
+import { UsuarioModule } from "src/usuarios/usuario.module";
+import { Usuario } from "src/usuarios/usuario.entity";
 
 
 
 
 
 @Module({
-    imports: [TypeOrmModule.forFeature([PerfilProfesor, Clase])],
-    providers: [PerfilesProfesoresService],
+    imports: [TypeOrmModule.forFeature([PerfilProfesor, Clase, Usuario]),
+    UsuarioModule
+    ],
+    providers: [PerfilesProfesoresService, UsuariosService],
     controllers: [PerfilesProfesoresController],
-    exports: [PerfilesProfesoresService]
+    exports: [PerfilesProfesoresService, TypeOrmModule]
 })
 export class PerfilesProfesoresModule{}
